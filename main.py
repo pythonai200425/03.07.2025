19e5cfe5ea45680f51fee5f6856145cd0153452e
pythonai250824 <pythonai250824@gmail.com>
2025-07-03 18:56:36 +0300

    push

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 
 import sqlite3
+import os
+
+# check if file exists
+if os.path.exists('db1.db'):
+    os.remove('db1.db')  # delete file
 
 # connect to db
 # if does not exist -> create + connect
@@ -14,7 +19,7 @@ cursor = conn.cursor()
 
 # Create table
 cursor.execute('''
-CREATE TABLE COMPANY(
+CREATE TABLE IF NOT EXISTS COMPANY(
     ID INT PRIMARY KEY NOT NULL,
     NAME TEXT NOT NULL default 'unknown',
     AGE INT NOT NULL,
@@ -23,15 +28,47 @@ CREATE TABLE COMPANY(
 );
 ''')
 
-cursor.execute('''
+#  1
+#  unsafe
+# cursor.execute('''
+# INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
+# VALUES
+#  (1, 'Paul', 32, 'California', 20000.00 ),
+#  (2, 'Allen', 25, 'Texas', 15000.00 ),
+#  (3, 'Teddy', 23, 'Norway', 20000.00 ),
+#  (4, 'Mark', 25, 'Rich-Mond ', 65000.00 ),
+#  (5, 'David', 27, 'Texas', 85000.00 ),
+#  (6, 'Kim', 22, 'South-Hall', 45000.00 );
+# ''')
+
+# 2
+# safe - one by one
+# cursor.execute('''
+# INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
+# VALUES (?, ?, ?, ?, ?);
+# ''', (1, 'Paul', 32, 'California', 20000.00))
+# cursor.execute('''
+# INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
+# VALUES (?, ?, ?, ?, ?);
+# ''', (2, 'Allen', 25, 'Texas', 15000.00 ))
+
+# 3
+# safe - multiple query
+data = [
+    (1, 'Paul', 32, 'California', 20000.00),
+    (2, 'Allen', 25, 'Texas', 15000.00),
+    (3, 'Teddy', 23, 'Norway', 20000.00),
+    (4, 'Mark', 25, 'Rich-Mond ', 65000.00),
+    (5, 'David', 27, 'Texas', 85000.00),
+    (6, 'Kim', 22, 'South-Hall', 45000.00),
+]
+cursor.executemany('''
 INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
-VALUES 
- (1, 'Paul', 32, 'California', 20000.00 ),
- (2, 'Allen', 25, 'Texas', 15000.00 ),
- (3, 'Teddy', 23, 'Norway', 20000.00 ),
- (4, 'Mark', 25, 'Rich-Mond ', 65000.00 ),
- (5, 'David', 27, 'Texas', 85000.00 ),
- (6, 'Kim', 22, 'South-Hall', 45000.00 );
+VALUES (?, ?, ?, ?, ?);
+''', data)
+
+cursor.execute('''
+UPDATE COMPANY SET ADDRESS = 'Texas' WHERE ID = 6;
 ''')
 
 conn.commit()  # write changes
